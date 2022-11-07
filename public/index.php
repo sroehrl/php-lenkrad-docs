94a09caa7f19d51e478f8f39cc18fdd1152d38ae
sroehrl <stefan.roehrl@gmail.com>
2022-11-06 19:55:14 -0500

    templating update

--- public/index.php
+++ public/index.php
@@ -7,8 +7,8 @@ use Neoan\NeoanApp;
 use Neoan\Request\Request;
 use Neoan\Store\Store;
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', true);
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors', true);
 
 
 $start = microtime(true);
