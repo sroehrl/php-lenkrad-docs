0bef1ab0625cf395ec1c7411259b4846a45f6fca
neoan <stefan.roehrl@gmail.com>
2023-09-10 14:05:50 -0400

    another day, another run

--- src/Docs/Deploy.php
+++ src/Docs/Deploy.php
@@ -6,7 +6,7 @@ use Neoan\Routing\Interfaces\Routable;
 use Neoan\Routing\Attributes\Web;
 use Neoan\Store\Store;
 
-#[Web('/docs/deploy', '/docs/in-development.html')]
+#[Web('/docs/deploy', '/docs/deploy.html')]
 class Deploy implements Routable
 {
     public function __invoke(): array
